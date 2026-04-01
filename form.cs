3fd0034d931b6e80ebc8b77c12616469f220703c
JSpanjaard07 <joeyamgelo123@outlook.com>
2026-04-01 10:35:38 +0200

    Commit

--- form.cs
+++ form.cs
@@ -1,6 +1,17 @@
-public partial class Form1 : Form
+using System;
+using System.Windows.Forms;
 
-public Form1() {
-    InitializeComponent();
-    Card card1 = new Card(Rank.ACE, Suit,SPADES)
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Card card1 = new Card(Rank.ACE, Suit.SPADES);
+
+            MessageBox.Show(card1.ToString() + " (Value: " + card1.Value + ")");
+        }
+    }
 }
